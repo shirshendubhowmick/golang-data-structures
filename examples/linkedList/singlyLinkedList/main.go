0aa32ef75c81edba369ace61973815fcfa83fddf
Shirshendu Bhowmick <shirshendu.bhowmick3@gmail.com>
2020-09-28 02:04:40 +0530

    example update singly linked list

--- examples/linkedList/singlyLinkedList/main.go
+++ examples/linkedList/singlyLinkedList/main.go
@@ -9,12 +9,12 @@ func main() {
 	list := sll.InitList(4)
 
 	list.Append(5)
-	list.Append(6)
-	nodeTobeReplace := list.Append(7)
+	nodeRef0 := list.Append(6)
+	list.Append(7)
 	list.Append(8)
 
 	list.Prepend(3)
-	list.Prepend(2)
+	nodeRef1 := list.Prepend(2)
 	list.Prepend(1)
 	list.Prepend(0)
 
@@ -27,13 +27,26 @@ func main() {
 	fmt.Println(list.Get(4))
 
 	fmt.Println(list.IndexOf(8))
+
 	list.ReplaceByIndex(7, 99)
 	list.Print()
+
 	fmt.Println(list.ToSlice())
 	fmt.Println(list.Length())
 
+	fmt.Println(list.InsertAt(8, 200))
 	list.Print()
 
-	fmt.Println(list.ReplaceByNode(nodeTobeReplace, 1000))
+	_, nodeRef2, _ := list.ReplaceByNode(nodeRef1, 999)
 	list.Print()
+
+	list.RemoveByNode(nodeRef2)
+	list.Print()
+
+	list.InsertAfter(nodeRef0, 500)
+	list.Print()
+
+	list.InsertBefore(nodeRef0, 600)
+	list.Print()
+
 }
